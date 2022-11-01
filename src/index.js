8d46f6a9090af6d41fc2605af538c652cbd750eb
ulises Gomez <ulisescap123@gmail.com>
2022-11-01 20:18:55 -0300

    h

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './containers/App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter basename='Weather-App'> 
+  <BrowserRouter> 
   <App />
   </BrowserRouter>
  ,
